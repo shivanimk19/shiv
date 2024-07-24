19fdb5b789874b1628349bbdd4f80a5a1dd27df0
shivanimk19 <89935319+shivanimk19@users.noreply.github.com>
2024-07-24 11:02:06 -0400

    Update setup.py small update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='shiv',  
-    version='0.1.3',  
+    version='0.1.4',  
     description='A package to perform sanity checks on attribution methods.',
     packages=find_packages(),  
     install_requires=[],
