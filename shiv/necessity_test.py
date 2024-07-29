38b319d51261ed5c0197ccda2a08609ba1e3cb88
shivanimk19 <89935319+shivanimk19@users.noreply.github.com>
2024-07-29 12:23:48 -0400

    Update necessity_test.py percent changes

--- shiv/necessity_test.py
+++ shiv/necessity_test.py
@@ -5,12 +5,13 @@ import tfomics
 from deeplift import dinuc_shuffle
 
 class Necessity:
-    PERCENTILES = [0, 5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 95, 100]
+    DEFAULT_PERCENTILES = [0, 5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 95, 100]
 
-    def __init__(self, model, class_index, num_iterations=20):
+    def __init__(self, model, class_index, num_iterations=20, percentiles=None):
         self.model = model
         self.class_index = class_index
         self.num_iterations = num_iterations
+        self.PERCENTILES = percentiles if percentiles is not None else self.DEFAULT_PERCENTILES
 
     def calculate_thresholds(self, all_attr_scores):
         max_abs_value = np.max(np.abs(all_attr_scores))
